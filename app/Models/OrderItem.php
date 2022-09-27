388c37c9cedbc37445a38211716f231d4623a50c
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2022-09-27 13:21:09 +0500

    auth

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class OrderItem extends Model
+class OrderItem extends Pivot
 {
     use HasFactory;
+    protected $fillable = ['product_id', 'quantity', 'price'];
 }
