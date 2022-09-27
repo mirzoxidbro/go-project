453d0e24c3f5becc57b2d938013c2084f5a8759b
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2022-09-27 17:34:42 +0500

    fix

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class OrderItem extends Pivot
+class OrderItem extends Model
 {
     use HasFactory;
     protected $fillable = ['product_id', 'quantity', 'price'];
